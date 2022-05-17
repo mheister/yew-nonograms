e2efc20b47416f021fb29fed859c8d5e7f3c8ea8
Marcel Heistermann <mheistermann@gmx.de>
2022-05-17 21:43:14 +0200

    switch to struct component

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,38 @@ struct Model {
     state1: i32,
 }
 
-#[function_component(App)]
-fn app() -> Html {
-    let state = use_state(|| Model { state1: 0 });
-    let onclick = {
-        let state = state.clone();
-        Callback::from(move |_| {
-            state.set(Model {
-                state1: state.state1 + 1,
-            })
-        })
-    };
+enum ModelMsg {
+    PlusOne,
+}
 
-    html! {
-        <div>
-            <button {onclick}>{"+1"}</button>
-            <p>{state.state1}</p>
-        </div>
+impl Component for Model {
+    type Message = ModelMsg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { state1: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            ModelMsg::PlusOne => {
+                self.state1 += 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| ModelMsg::PlusOne)}>{"+1"}</button>
+                <p>{self.state1}</p>
+            </div>
+        }
     }
 }
 
 fn main() {
-    yew::start_app::<App>();
+    yew::start_app::<Model>();
 }
