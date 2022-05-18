314d5a7da33eb616a275383fa771090ec1969367
Marcel Heistermann <mheistermann@gmx.de>
2022-05-18 22:39:09 +0200

    render some zeros and rectangles as hints and cells

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::iproduct;
 use yew::prelude::*;
 
 struct Model {
@@ -31,26 +32,59 @@ impl Component for Model {
         let n_rows = 10 + self.state1;
         let n_hints = 3;
         assert!(n_rows > n_hints);
-        let items = (n_hints..n_rows).map(|x| 500 / n_rows * x).collect::<Vec<_>>();
+        let width = 500;
+        let cell_width = width / n_rows;
+
+        let grid_svg = (n_hints..n_rows)
+            .map(|xi| cell_width * xi)
+            .into_iter()
+            .map(|x| {
+                html! {
+                    <>
+                        <line x1={x.to_string()} y1="0"
+                              x2={x.to_string()} y2="500"
+                              style="stroke:#DADADA;stroke-width:2" />
+                        <line x1="0" y1={x.to_string()}
+                              x2="500" y2={x.to_string()}
+                              style="stroke:#DADADA;stroke-width:2" />
+                    </>
+                }
+            })
+            .collect::<Html>();
+
+        let hints_svg = iproduct!(n_hints..n_rows, 0..n_hints)
+            .chain(iproduct!(0..n_hints, n_hints..n_rows))
+            .map(|(xi, yi)| {
+                (
+                    cell_width * xi + cell_width / 2 - 4,
+                    cell_width * yi + cell_width / 2 + 6,
+                )
+            })
+            .map(|(x, y)| {
+                html! {
+                    <text x={x.to_string()} y={y.to_string()} fill="blue">{"0"}</text>
+                }
+            })
+            .collect::<Html>();
+
+        let cell_svg = |xi: i32, yi: i32| {
+            let x = cell_width * xi + 1;
+            let y = cell_width * yi + 1;
+            let rect_width = cell_width - 2;
+            html! {
+                <rect x={x.to_string()} y={y.to_string()}
+                 width={rect_width.to_string()}
+                 height={rect_width.to_string()}
+                 style="fill:#8D6D6D;stroke-width:1;stroke:#6E4E4E" />
+            }
+        };
+        let cells_svg = iproduct!(n_hints..n_rows, n_hints..n_rows)
+            .map(|(xi, yi)| cell_svg(xi, yi))
+            .collect::<Html>();
 
         html! {
             <div>
-                <svg width="500" height="500">
-                {
-                    items.into_iter().map(|i| {
-                        html! {
-                            <>
-                                <line x1={i.to_string()} y1="0"
-                                      x2={i.to_string()} y2="500"
-                                      style="stroke:#DADADA;stroke-width:2" />
-                                <line x1="0" y1={i.to_string()}
-                                      x2="500" y2={i.to_string()}
-                                      style="stroke:#DADADA;stroke-width:2" />
-                            </>
-                        }
-                    }).collect::<Html>()
-                }
-                </svg>
+                <svg width="500" height="500">{grid_svg}{hints_svg}{cells_svg}</svg>
                 <p></p>
                 <button onclick={link.callback(|_| ModelMsg::PlusOne)}>{"+1"}</button>
                 <p>{self.state1}</p>
