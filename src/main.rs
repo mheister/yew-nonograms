2f5d57950ce340a0942931a5ff34b720e891c1b0
Marcel Heistermann <mheistermann@gmx.de>
2022-05-18 20:47:48 +0200

    render basic grid

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,31 @@ impl Component for Model {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
+
+        let n_rows = 10 + self.state1;
+        let n_hints = 3;
+        assert!(n_rows > n_hints);
+        let items = (n_hints..n_rows).map(|x| 500 / n_rows * x).collect::<Vec<_>>();
+
         html! {
             <div>
+                <svg width="500" height="500">
+                {
+                    items.into_iter().map(|i| {
+                        html! {
+                            <>
+                                <line x1={i.to_string()} y1="0"
+                                      x2={i.to_string()} y2="500"
+                                      style="stroke:#DADADA;stroke-width:2" />
+                                <line x1="0" y1={i.to_string()}
+                                      x2="500" y2={i.to_string()}
+                                      style="stroke:#DADADA;stroke-width:2" />
+                            </>
+                        }
+                    }).collect::<Html>()
+                }
+                </svg>
+                <p></p>
                 <button onclick={link.callback(|_| ModelMsg::PlusOne)}>{"+1"}</button>
                 <p>{self.state1}</p>
             </div>
