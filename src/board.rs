b453255f0283c14ccc7e119bb1924364a17281e2
Marcel Heistermann <mheistermann@gmx.de>
2022-05-24 23:19:50 +0200

    add board model with hint generation

---
+++ src/board.rs
@@ -0,0 +1,185 @@
+use itertools::Itertools;
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum FieldCell {
+    Empty = 0,
+    Filled = 1,
+    Marked = 2,
+}
+
+impl Default for FieldCell {
+    fn default() -> Self {
+        FieldCell::Empty
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub struct HintCell {
+    pub number: u8,    // 0 represents empty field
+    pub crossed: bool, // player can mark hints
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Grid<T> {
+    width: usize,
+    cells: Vec<T>,
+}
+
+pub struct Board {
+    width: usize,
+    field: Grid<FieldCell>,
+    solution: Grid<FieldCell>,
+    col_hints: Grid<HintCell>,
+    row_hints: Grid<HintCell>,
+}
+
+impl<T> std::ops::Index<usize> for Grid<T> {
+    type Output = [T];
+    fn index(&self, index: usize) -> &Self::Output {
+        let rowstart = self.width as usize * index;
+        &self.cells[rowstart..rowstart + self.width as usize]
+    }
+}
+
+impl<T> std::ops::IndexMut<usize> for Grid<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        let rowstart = self.width as usize * index;
+        &mut self.cells[rowstart..rowstart + self.width as usize]
+    }
+}
+
+impl<T: Default + Clone + Copy> Grid<T> {
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            cells: [T::default()].repeat(width * height),
+        }
+    }
+    pub fn from_flat(width: usize, cells: &[T]) -> Self {
+        let mut cells: Vec<T> = cells.into();
+        cells.resize(width * width, T::default());
+        Self { width, cells }
+    }
+}
+
+impl Board {
+    fn generate_col_hints(&mut self) {
+        for col in 0..self.width {
+            let counts: Vec<u8> = (0..self.width)
+                .map(|idx| self.solution[idx][col])
+                .map(|cell| (cell, 1u8))
+                .coalesce(|(cell1, count1), (cell2, count2)| {
+                    if cell1 == cell2 {
+                        Ok((cell1, count1 + count2))
+                    } else {
+                        Err(((cell1, count1), (cell2, count2)))
+                    }
+                })
+                .filter(|(cell, _)| *cell == FieldCell::Filled)
+                .map(|(_, count)| count)
+                .collect();
+            for zip in (0..self.hint_len()).rev().zip_longest(counts.iter().rev()) {
+                match zip {
+                    itertools::EitherOrBoth::Left(idx) => {
+                        self.col_hints[idx][col].number = 0;
+                    }
+                    itertools::EitherOrBoth::Both(idx, val) => {
+                        self.col_hints[idx][col].number = *val;
+                    }
+                    itertools::EitherOrBoth::Right(_) => {}
+                }
+            }
+        }
+    }
+
+    fn generate_row_hints(&mut self) {
+        for row in 0..self.width {
+            let counts: Vec<u8> = self.solution[row]
+                .iter()
+                .map(|cell| (*cell, 1u8))
+                .coalesce(|(cell1, count1), (cell2, count2)| {
+                    if cell1 == cell2 {
+                        Ok((cell1, count1 + count2))
+                    } else {
+                        Err(((cell1, count1), (cell2, count2)))
+                    }
+                })
+                .filter(|(cell, _)| *cell == FieldCell::Filled)
+                .map(|(_, count)| count)
+                .collect();
+            for zip in self.row_hints[row]
+                .iter_mut()
+                .rev()
+                .zip_longest(counts.iter().rev())
+            {
+                match zip {
+                    itertools::EitherOrBoth::Left(cell) => {
+                        cell.number = 0;
+                    }
+                    itertools::EitherOrBoth::Both(cell, val) => {
+                        cell.number = *val;
+                    }
+                    itertools::EitherOrBoth::Right(_) => {}
+                }
+            }
+        }
+    }
+
+    fn generate_hints(&mut self) {
+        self.generate_col_hints();
+        self.generate_row_hints();
+    }
+}
+
+impl Board {
+    pub fn new() -> Board {
+        let width = 10;
+        let height = width;
+        let fields = (0..width * width).map(|i| {
+            if i % 3 == 0 || i % 7 == 0 || i % 11 == 0 {
+                FieldCell::Filled
+            } else {
+                FieldCell::Empty
+            }
+        });
+        let hint_len = (width + 1) / 2;
+        let mut result = Board {
+            width,
+            field: Grid::new(width, width),
+            solution: Grid::from_flat(width, &Vec::<FieldCell>::from_iter(fields)),
+            col_hints: Grid::new(width, hint_len),
+            row_hints: Grid::new(hint_len, height),
+        };
+        result.generate_hints();
+        result
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn hint_len(&self) -> usize {
+        (self.solution.width + 1) / 2
+    }
+
+    pub fn col_hint(&self, col: usize, pos: usize) -> HintCell {
+        self.col_hints[pos][col]
+    }
+
+    pub fn row_hint(&self, row: usize, pos: usize) -> HintCell {
+        self.row_hints[row][pos]
+    }
+
+    pub fn solution(&self) -> &Grid<FieldCell> {
+        &self.solution
+    }
+
+    pub fn fill(&mut self, row: usize, col: usize) {
+        self.field[row][col] = FieldCell::Filled;
+    }
+
+    pub fn mark(&mut self, row: usize, col: usize) {
+        self.field[row][col] = FieldCell::Marked;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
+mod board;
+
+use board::Board;
 use itertools::iproduct;
 use yew::prelude::*;
 
-struct Model {
+struct NonogramGame {
     state1: i32,
+    board: Board,
 }
 
 enum ModelMsg {
     PlusOne,
 }
 
-impl Component for Model {
+impl Component for NonogramGame {
     type Message = ModelMsg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { state1: 0 }
+        Self {
+            state1: 0,
+            board: Board::new(),
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -29,49 +36,58 @@ impl Component for Model {
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
 
-        let n_rows = 10 + self.state1;
-        let n_hints = 3;
-        assert!(n_rows > n_hints);
-        let target_width = 500;
-        let cell_width = target_width / n_rows;
-        let width = cell_width * n_rows;
+        let n_hints = self.board.hint_len();
+        let n_field_rows = self.board.width();
+        let n_rows = n_hints + n_field_rows;
+        let target_width_px = 500;
+        let cell_width_px = target_width_px / n_rows;
+        let width_px = cell_width_px * n_rows;
 
         let grid_svg = (n_hints..n_rows)
-            .map(|xi| cell_width * xi)
+            .map(|xi| cell_width_px * xi)
             .into_iter()
             .map(|x| {
                 html! {
                     <>
                         <line x1={x.to_string()} y1="0"
-                              x2={x.to_string()} y2={width.to_string()}
+                              x2={x.to_string()} y2={width_px.to_string()}
                               style="stroke:#DADADA;stroke-width:2" />
                         <line x1="0" y1={x.to_string()}
-                              x2={width.to_string()} y2={x.to_string()}
+                              x2={width_px.to_string()} y2={x.to_string()}
                               style="stroke:#DADADA;stroke-width:2" />
                     </>
                 }
             })
             .collect::<Html>();
 
-        let hints_svg = iproduct!(n_hints..n_rows, 0..n_hints)
-            .chain(iproduct!(0..n_hints, n_hints..n_rows))
-            .map(|(xi, yi)| {
-                (
-                    cell_width * xi + cell_width / 2 - 4,
-                    cell_width * yi + cell_width / 2 + 6,
-                )
-            })
-            .map(|(x, y)| {
-                html! {
-                    <text x={x.to_string()} y={y.to_string()} fill="blue">{"0"}</text>
-                }
-            })
-            .collect::<Html>();
+        let hints_svg = {
+            let col_hints = iproduct!(0..n_field_rows, 0..n_hints)
+                .map(|(xi, yi)| (n_hints + xi, yi, self.board.col_hint(xi, yi).number));
+            let row_hints = iproduct!(0..n_hints, 0..n_field_rows)
+                .map(|(xi, yi)| (xi, n_hints + yi, self.board.row_hint(yi, xi).number));
+            col_hints
+                .chain(row_hints)
+                .filter(|(_,_,val)| *val != 0u8)
+                .map(|(xi, yi, val)| {
+                    (
+                        cell_width_px * xi + cell_width_px / 2 - 4,
+                        cell_width_px * yi + cell_width_px / 2 + 6,
+                        val,
+                    )
+                })
+                .map(|(x, y, val)| (x.to_string(), y.to_string(), val.to_string()))
+                .map(|(x, y, val)| {
+                    html! {
+                        <text {x} {y} fill="blue">{val}</text>
+                    }
+                })
+                .collect::<Html>()
+        };
 
-        let cell_svg = |xi: i32, yi: i32| {
-            let x = cell_width * xi + 1;
-            let y = cell_width * yi + 1;
-            let rect_width = cell_width - 2;
+        let cell_svg = |xi: usize, yi: usize| {
+            let x = cell_width_px * xi + 1;
+            let y = cell_width_px * yi + 1;
+            let rect_width = cell_width_px - 2;
             html! {
                 <rect x={x.to_string()} y={y.to_string()}
                  width={rect_width.to_string()}
@@ -79,13 +95,15 @@ impl Component for Model {
                  style="fill:#8D6D6D;stroke-width:1;stroke:#6E4E4E" />
             }
         };
-        let cells_svg = iproduct!(n_hints..n_rows, n_hints..n_rows)
-            .map(|(xi, yi)| cell_svg(xi, yi))
+        let cells_svg = iproduct!(0..n_field_rows, 0..n_field_rows)
+            .map(|(xi, yi)| (xi + n_hints, yi + n_hints, self.board.solution()[yi][xi]))
+            .filter(|(_, _, cell)| cell.eq(&board::FieldCell::Filled))
+            .map(|(xi, yi, _)| cell_svg(xi, yi))
             .collect::<Html>();
 
         html! {
             <div>
-                <svg width={target_width.to_string()} height={target_width.to_string()}>
+                <svg width={target_width_px.to_string()} height={target_width_px.to_string()}>
                     {grid_svg}{hints_svg}{cells_svg}
                 </svg>
                 <p></p>
@@ -97,5 +115,5 @@ impl Component for Model {
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<NonogramGame>();
 }
