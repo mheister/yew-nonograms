280536cd9d299db494c368eeb4ed72049c819260
Marcel Heistermann <mheistermann@gmx.de>
2023-01-19 19:55:41 +0100

    use select element for puzzle width

--- src/components/setting_panel.rs
+++ src/components/setting_panel.rs
@@ -16,49 +16,38 @@ pub struct SettingPanelProps {
 #[function_component(SettingPanel)]
 pub fn setting_panel(props: &SettingPanelProps) -> Html {
     let puzzle_state = props.puzzle.clone();
-    let puzzle_width_state = props.puzzle_width.clone();
-    let puzzle_width_set_value_state = use_state(|| *puzzle_width_state);
-    let unapplied_change = *puzzle_width_state != *puzzle_width_set_value_state;
-    let puzzle_width_set_value_oninput = {
-        let puzzle_width_set_value_state = puzzle_width_set_value_state.clone();
-        Callback::from(move |e: InputEvent| {
+    let width_state = props.puzzle_width.clone();
+    let width = *width_state;
+    let width_onchange = {
+        let puzzle = props.puzzle.clone();
+        Callback::from(move |e: Event| {
             let target: EventTarget =
                 e.target().expect("Error: No target on oninput event");
             let setval = target.unchecked_into::<HtmlInputElement>().value();
-            if let Ok(value) = setval.parse() {
-                puzzle_width_set_value_state.set(value);
-            }
-        })
-    };
-    let apply = {
-        let puzzle = props.puzzle.clone();
-        let puzzle_width_set_value_state = puzzle_width_set_value_state.clone();
-        move || {
-            let new_width = *puzzle_width_set_value_state;
-            puzzle_width_state.set(new_width);
+            let new_width = setval.parse().expect("Could not parse width");
+            width_state.set(new_width);
             log::info!("Increasing puzzle width to {new_width}");
             let mut grid = BoardModel::from_serialized_solution(puzzle.as_ref());
             grid.resize(new_width);
             puzzle_state.set(grid.solution_ref().serialize_base64())
-        }
+        })
     };
-    let apply_click = {
-        let apply = apply.clone();
-        Callback::from(move |_| apply())
-    };
-    let apply_onchange = Callback::from(move |_| apply());
     html! {
         <>
             <div class="panel">
                 <h3>{"Set a Nonogram"}</h3>
-                <label>{"Width:"}
-                    <input type={"text"} id={"puzzle_width_input"}
-                     value={puzzle_width_set_value_state.to_string()} size={4}
-                     oninput={puzzle_width_set_value_oninput}
-                     onchange={apply_onchange}
-                     />
-                </label>
-                if unapplied_change {<button onclick={apply_click}>{"apply"}</button>}
+                <label for="puzzle_width_input">{"Width:"}</label>
+                <select id={"puzzle_width_input"} onchange={width_onchange}>
+                    {
+                        (5..=25).map(|w| html!{
+                            <option
+                                value={w.to_string()}
+                                selected={w == width}>
+                               {w.to_string()}
+                            </option>
+                        }).collect::<Html>()
+                    }
+                </select>
             // <p>{(*props.puzzle).clone()}</p>
             <p>
                 <Link<Route> to={Route::Set{puzzle: (*props.puzzle).clone()}}>
